f7f628b55842d532be5a2450b905cba711761245
amankumar110 <115738540+amankumar110@users.noreply.github.com>
2022-12-28 01:34:18 -0800

    complete  work

---
+++ script.js
@@ -0,0 +1,34 @@
+const numInputOne = document.getElementById("num1-input");
+const operatorInput = document.getElementById("operator-input");
+const numInputTwo = document.getElementById("num2-input");
+const submitBtn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+const add = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let num1 = numInputOne.value;
+  let operator = operatorInput.value;
+  let num2 = numInputTwo.value;
+  let ans = 0;
+  if (operator == "+") {
+    ans = add(num1, num2);
+  } else if (operator == "-") {
+    ans = sub(num1, num2);
+  } else if (operator == "*") {
+    ans = multiply(num1, num2);
+  } else if (operator == "/") {
+    ans = divide(num1, num2);
+  }
+  else {
+    ans = "Enter valid operator only"
+  }
+  result.textContent = ans;
+});
